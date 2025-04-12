4c76177e0c3f4ef10ec57cd3443afec748a055e7
OGKodingKat <katmarie9628@gmail.com>
2025-04-11 21:01:01 -0500

    render?

--- version-5/src/App.jsx
+++ version-5/src/App.jsx
@@ -1,45 +1,29 @@
-import './App.css'
+import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import SavedCountry from './pages/SavedCountry.jsx';
 import CountryPage from './pages/CountryPage.jsx';
 import UserForm from './custom-components/UserForm.jsx';
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCwDxtdjMyDcsqLAO3GyJioIgO2Gjk7wsE",
-  authDomain: "country-api-3043a.firebaseapp.com",
-  projectId: "country-api-3043a",
-  storageBucket: "country-api-3043a.firebasestorage.app",
-  messagingSenderId: "297463227591",
-  appId: "1:297463227591:web:96fe6d47c94fbad3c68ae2",
-  measurementId: "G-HB0HKML5WR"
-};
-
 
 function App() {
+  const API_URL = "https://rest-countries-api-5q0p.onrender.com";
 
-  const app = initializeApp(firebaseConfig);
-console.log(app);
   return (
     <div>
       <nav>
         <ul>
-          <li>
-            <Link to="/">Where in the World?</Link>
-          </li>
-          <li>
-            <Link to="/savedcountry">Saved Country</Link>
-          </li>
+          <li><Link to="/">Where in the World?</Link></li>
+          <li><Link to="/savedcountry">Saved Country</Link></li>
+          <li><Link to="/userform">User Form</Link></li>
           <li>Dark Mode</li>
         </ul>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/" element={<UserForm />} />
+        <Route path="/userform" element={<UserForm />} />
         <Route path="/savedcountry" element={<SavedCountry />} />
-        <Route path="/countrypage/:country" element ={<CountryPage />} />
+        <Route path="/countrypage/:country" element={<CountryPage />} />
       </Routes>
     </div>
   );
