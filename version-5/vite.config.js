f2cea0d8a9241d19f83b7799b18e33ff0e3619be
OGKodingKat <katmarie9628@gmail.com>
2025-04-12 21:26:47 -0500

    version 5 render?

--- version-5/vite.config.js
+++ version-5/vite.config.js
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': { 
-        target:'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
-  plugins: [react()],
+ server: {
+   proxy: {
+     '/api': {
+       target:'https://rest-countries-api-5q0p.onrender.com',
+       changeOrigin: true,
+       secure: false,
+       rewrite: (path) => path.replace(/^\/api/, '')
+     }
+   }
+ },
+ plugins: [react()],
 })
