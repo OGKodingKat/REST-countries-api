90ddae29455d6befaed14b58a7208ca9bf23752e
OGKodingKat <katmarie9628@gmail.com>
2025-04-14 11:49:24 -0500

    fic userform issue

--- version-5/src/App.jsx
+++ version-5/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import SavedCountry from './pages/SavedCountry.jsx';
 import CountryPage from './pages/CountryPage.jsx';
-import UserForm from './custom-components/UserForm.jsx';
+
 
 function App() {
   const API_URL = "https://rest-countries-api-5q0p.onrender.com";
@@ -14,14 +14,12 @@ function App() {
         <ul>
           <li><Link to="/">Where in the World?</Link></li>
           <li><Link to="/savedcountry">Saved Country</Link></li>
-          <li><Link to="/userform">User Form</Link></li>
           <li>Dark Mode</li>
         </ul>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/userform" element={<UserForm />} />
         <Route path="/savedcountry" element={<SavedCountry />} />
         <Route path="/countrypage/:country" element={<CountryPage />} />
       </Routes>
