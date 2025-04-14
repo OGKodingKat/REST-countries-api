0e01265439bdbaf8a06fdab3e49559aa8e5c8c60
OGKodingKat <katmarie9628@gmail.com>
2025-04-14 15:54:16 -0500

    update tables and schemas

--- database-schema.sql
+++ database-schema.sql
@@ -1,15 +1,15 @@
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
-    name VARCHAR(50) UNIQUE NOT NULL,
+    name VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     country VARCHAR(50) NOT NULL,
-    bio VARCHAR(150),
+    bio VARCHAR(150)
 );
 
 CREATE TABLE saved_countries (
     save_id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
-    country_name VARCHAR(100) NOT NULL,
+    user_id INT,
+    country_name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE visit_counts (
@@ -18,10 +18,10 @@ CREATE TABLE visit_counts (
     count INT DEFAULT 1
 );
 
-INSERT INTO users (username, email) VALUES
-('katmarie', 'kat@example.com'),
-('devguy', 'dev@example.com'),
-('coderchick', 'coder@example.com');
+INSERT INTO users (name, email, country, bio) VALUES
+('katmarie', 'kat@example.com', 'Berlin', 'poop'),
+('devguy', 'dev@example.com', 'Germany', 'giggle'),
+('coderchick', 'coder@example.com', 'Hungary', 'hungry');
 
 INSERT INTO saved_countries (user_id, country_name) VALUES
 (1, 'Japan'),
@@ -37,6 +37,11 @@ INSERT INTO visit_counts (country_name, count) VALUES
 ('Germany', 1),
 ('Brazil', 1);
 
+SELECT users.username 
+FROM users 
+JOIN saved_countries ON users.user_id = saved_countries.user_id 
+WHERE saved_countries.country_name = 'Japan';
+
 SELECT country_name, saved_at 
 FROM saved_countries 
 WHERE user_id = (SELECT user_id FROM users WHERE username = 'katmarie');
@@ -50,11 +55,3 @@ SELECT *
 FROM saved_countries 
 WHERE user_id = (SELECT user_id FROM users WHERE username = 'katmarie') 
 AND country_name = 'Japan';
-
-INSERT INTO saved_countries (user_id, country_name) 
-VALUES ((SELECT user_id FROM users WHERE username = 'katmarie'), 'Italy');
-
-SELECT users.username 
-FROM users 
-JOIN saved_countries ON users.user_id = saved_countries.user_id 
-WHERE saved_countries.country_name = 'Japan';
